146c7d18aa2f41d4d9efbfbe41e017537123bded
Bruno Nakahara <Bruno_Nakahara95@yahoo.com>
2022-04-07 14:24:13 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosAPI = axios.create({
-  baseURL: 'http://192.168.0.10:3000/v1',
+  baseURL: 'http://xxxx:3000/v1',
   headers: {
     'Content-Type': 'application/json',
   },
